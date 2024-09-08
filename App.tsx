1378cb1cf2d7f424753e16d8f37b45a6bd743acd
jonathandr9 <jonathandias96@gmail.com>
2024-09-08 18:58:33 -0300

    Import font roboto in app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components';
 import { Groups } from "@screens/Groups";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from './src/theme/'
+import {Loading} from '@components/Loading'
 
-export default function App() {
+export default function App() {  
+  
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold}); 
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,10 @@
 import {
-    Container, 
-    Title
+    Container
 } from './styles'
 
 export function Groups() {
     return (
         <Container>
-            <Title>
-                Groups
-            </Title>
         </Container>
     );
 }
