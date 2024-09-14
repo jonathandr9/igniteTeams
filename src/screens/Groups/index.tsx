6a2ab3d4eec578ce2076435b1c0c0c8da0122e96
jonathandr9 <jonathandias96@gmail.com>
2024-09-14 18:12:21 -0300

    List Empty Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,6 +2,7 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 import {
     Container
 } from './styles'
@@ -9,7 +10,7 @@ import { useState } from 'react';
 
 export function Groups() {
 
-    const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Alura', 'Desenvolvedor.io'])
+    const [groups, setGroups] = useState<string[]>([])
 
     return (
         <Container>
@@ -19,12 +20,16 @@ export function Groups() {
                 subtitle='jogue com a sua turma'
             />
             <FlatList 
-                data={groups}
+                data={groups} 
                 keyExtractor={(item) => item}
                 renderItem={({item}) => (
                     <GroupCard 
                         title={item}
                     />                    
+                )}
+                contentContainerStyle = {groups.length  === 0 && {flex: 1}}
+                ListEmptyComponent={(item) => (
+                    <ListEmpty message="Que tal cadastrar a primeira Turmar?"/>
                 )}
             />
 
