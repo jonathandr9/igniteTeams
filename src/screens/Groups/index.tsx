44c4bb91d47f36c353bca32c7ff2df3abc85615a
jonathandr9 <jonathandias96@gmail.com>
2024-09-14 19:18:29 -0300

    Component Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,6 +3,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 import {
     Container
 } from './styles'
@@ -31,6 +32,10 @@ export function Groups() {
                 ListEmptyComponent={(item) => (
                     <ListEmpty message="Que tal cadastrar a primeira Turmar?"/>
                 )}
+            />
+            <Button
+                // type='PRIMARY'
+                title="Criar Nova Turma"
             />
 
         </Container>
