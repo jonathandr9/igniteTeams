ddfc9985a0e9218469a50e3727e8bde78dc672fd
jonathandr9 <jonathandias96@gmail.com>
2024-09-14 22:49:28 -0300

    Input  Component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from "@components/Header";
 import { Container, Content, Icon} from "./styles";
 import { Button } from "@components/Button";
-import { Highlight } from '@components/Highlight'
+import { Highlight } from '@components/Highlight';
+import {Input} from '@components/Input';
 
 export function NewGroup(){
     return (
@@ -9,12 +10,22 @@ export function NewGroup(){
             <Header showBackButton/>
 
             <Content>
+
                 <Icon name="users"/>
+
                 <Highlight
                     title="Novas Turmas"
                     subtitle="crie a turma para adicionar as pessoas"
                 />
-                <Button title="Criar"/>
+
+                <Input
+                    placeholder="Nome da Turma"
+                />
+
+                <Button 
+                    title="Criar"
+                    style={{marginTop: 20}}
+                />
             </Content>
 
         </Container>
